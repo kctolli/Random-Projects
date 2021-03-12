2fa8d800be8e623d6e446afbf2ff974f4177f3b5
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2021-03-12 16:07:52 -0700

    fib

---
+++ fib.py
@@ -0,0 +1,15 @@
+def recur_fibo(n):
+    x = (n if n <= 1 else recur_fibo(n-1) + recur_fibo(n-2))
+    return x
+
+def main():
+    nterms = 0
+    while nterms <= 0:
+        nterms = int(input("Please enter a positive integer: "))
+        print("Fibonacci sequence:")
+        for i in range(nterms):
+            print(recur_fibo(i))
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()  
