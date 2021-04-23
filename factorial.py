592184d835f3b86f7b1cbc735fb4a46038e0b004
kctolli <kyle@tollivers.org>
2021-04-23 00:40:51 -0600

    Update at 00:40 on 04/23/2021

--- factorial.py
+++
@@ -1,9 +0,0 @@
-def factorial(x): return (1 if x == 1 else (x * factorial(x-1)))
-
-def main():
-    num = int(input("Input a value: "))
-    print("The factorial of", num, "is", factorial(num))
-
-if __name__ == "__main__":
-    # execute only if run as a script
-    main()  
